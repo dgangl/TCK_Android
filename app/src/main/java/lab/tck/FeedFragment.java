2b1038cf1269ecd8ec049050ae1ec9c13fd6e889
dgangl <davidgangl@icloud.com>
2019-05-23 08:39:52 +0200

    UI Feed/User and Phone Login with Code

--- app/src/main/java/lab/tck/FeedFragment.java
+++ app/src/main/java/lab/tck/FeedFragment.java
@@ -8,11 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FeedFragment extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_feed,container,false);
+
+
     }
+
+
 }
