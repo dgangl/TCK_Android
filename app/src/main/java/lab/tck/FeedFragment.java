672c99606f6bfeb373f8514671080fb15e401fe2
dgangl <davidgangl@icloud.com>
2019-06-01 11:36:49 +0200

    Revert "Revert "Revert "Merge branch 'master' into dgangl"""
    This reverts commit 185d1d9966a72dc2aaa3d05a919a0f8e2b4b6f96.

--- app/src/main/java/lab/tck/FeedFragment.java
+++ app/src/main/java/lab/tck/FeedFragment.java
@@ -1,6 +1,5 @@
 package lab.tck;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -40,21 +39,16 @@ private FirebaseFirestore db;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_feed, null);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_feed, null);
         feedList = root.findViewById(R.id.feeds);
 
-<<<<<<< HEAD
         System.out.println("Test2 Startet");
         eventArrayList = new ArrayList<>();
 
-=======
-        System.out.println("Starting Test 1");
->>>>>>> bb427663bff960c63f8a83abb1104254402f02d8
         BackendFeedDatabase be = new BackendFeedDatabase();
         be.loadAllEvents(new MyEntryArrayInterface() {
             @Override
             public void onCallback(List<Entry> entryList) {
-<<<<<<< HEAD
                 System.out.println("Test3" +entryList.size());
             }
         });
@@ -70,17 +64,6 @@ private FirebaseFirestore db;
 
         feedListAdapter = new FeedListAdapter(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, eventArrayList);
         feedList.setAdapter(feedListAdapter);
-=======
-                System.out.println("AAC" +entryList.size());
-
-
-
-                feedListAdapter = new FeedListAdapter(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, (ArrayList<Entry>) entryList);
-                feedList.setAdapter(feedListAdapter);
-            }
-        });
-
->>>>>>> bb427663bff960c63f8a83abb1104254402f02d8
         return root;
     }
 
--- app/src/main/java/lab/tck/FeedListAdapter.java
+++ app/src/main/java/lab/tck/FeedListAdapter.java
@@ -1,37 +1,21 @@
 package lab.tck;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.AssetManager;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Space;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import Backend.Entry;
-import Interfaces.MyBooleanCompletion;
 
 public class FeedListAdapter extends ArrayAdapter<Entry> {
     private final LayoutInflater inflater;
-    private final Context context;
     private int resource;
     private List<Entry> eventArrayList;
 
@@ -39,127 +23,16 @@ public class FeedListAdapter extends ArrayAdapter<Entry> {
         super(context, resource, object);
         this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.resource = resource;
-<<<<<<< HEAD
         this.eventArrayList = object;
-=======
-        this.EntryArrayList = object;
-        this.context = context;
->>>>>>> bb427663bff960c63f8a83abb1104254402f02d8
     }
 
     @NonNull
     @Override
-<<<<<<< HEAD
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         final Entry currentEvent = eventArrayList.get(position);
         View listItem = inflater.inflate(R.layout.adapter_feedlist, parent, false);
 
         //((TextView) listItem.findViewById(R.id.feed_title)).setText(currentEvent.getTitle());
-=======
-    public View getView(int position, @Nullable final View convertView, @NonNull ViewGroup parent) {
-        final Entry currentEntry = EntryArrayList.get(position);
-        final View listItem = inflater.inflate(R.layout.adapter_feedlist, parent, false);
-
-        ((TextView) listItem.findViewById(R.id.feed_title)).setText(currentEntry.getBeschreibung());
-        ((TextView) listItem.findViewById(R.id.feed_description)).setText(currentEntry.getType());
-        ((TextView) listItem.findViewById(R.id.feed_dateTime)).setText(currentEntry.getDateString());
-
-        //Start and end-time
-        SimpleDateFormat sdf = new SimpleDateFormat("HH");
-        int start_time = Integer.valueOf(sdf.format(currentEntry.getDatum()));
-        Double d = currentEntry.getDauer();
-
-        int end_time = start_time + d.intValue();
-
-        ((TextView) listItem.findViewById(R.id.feed_duration)).setText(start_time + ":00 - "+end_time + ":00 Uhr");
-
-        String plaetze = "Plätze: " + "\n";
-        plaetze = plaetze + currentEntry.getPlatz().get(0);
-
-        for (int i = 1; i < currentEntry.getPlatz().size(); i++) {
-            plaetze = plaetze + ", " + currentEntry.getPlatz().get(i);
-        }
-        Button b = listItem.findViewById(R.id.button);
-
-        if(currentEntry.getUserIsIn() == 1){
-
-            Space s = listItem.findViewById(R.id.space);
-
-            System.out.println("Fundst!");
-
-            s.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 0f));
-            b.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 0f));
-        }else{
-            b.setBackgroundColor(Color.TRANSPARENT);
-            b.setTextColor(Color.BLACK);
-            plaetze = "Teilnehmen?";
-            ImageView mImageView;
-            mImageView = listItem.findViewById(R.id.imageView2);
-            mImageView.setImageDrawable(MainActivity.image);
-
-            mImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    System.out.println("Fundst! a");
-                    AlertDialog.Builder bobsebuilder = new AlertDialog.Builder(MainActivity.cont)
-                            .setTitle("Beitreten?")
-                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    currentEntry.joinThisEvent(new MyBooleanCompletion() {
-                                        @Override
-                                        public void onCallback(boolean bool) {
-                                            System.out.println("User added successfully");
-                                        }
-                                    });
-                                }
-                            })
-                            .setNeutralButton("Vielleicht", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-
-                                    dialog.dismiss();
-
-                                    currentEntry.thinkAboutThisEvent(new MyBooleanCompletion() {
-                                        @Override
-                                        public void onCallback(boolean bool) {
-                                            System.out.println("User thinking successfully");
-                                        }
-                                    });
-                                }
-                            })
-                            .setNegativeButton("Absagen", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    currentEntry.leaveThisEvent(new MyBooleanCompletion() {
-                                        @Override
-                                        public void onCallback(boolean bool) {
-                                            System.out.println("User canceled event" +bool);
-                                        }
-                                    });
-
-                                }
-                            });
-
-                    bobsebuilder.show();
-
-
-
-
-                }
-            });
-
-
-        }
-
-
-
-        b.setText(plaetze);
-
-
-
-
->>>>>>> bb427663bff960c63f8a83abb1104254402f02d8
 
 
         return listItem;
